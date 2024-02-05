b3223dbfaabc7a8d74b887c8c2fb97fe464ab19b
kentoespdam <kentoes.pdam@gmail.com>
2024-02-05 10:30:45 +0700

    initialize repo

---
+++ src/main/java/id/perumdamts/cater/CaterApplication.java
@@ -0,0 +1,13 @@
+package id.perumdamts.cater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaterApplication.class, args);
+	}
+
+}
