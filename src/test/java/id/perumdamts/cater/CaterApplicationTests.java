1fc83410fd132ea9048007edbddfdb58eed1f4e2
kentoespdam <kentoes.pdam@gmail.com>
2024-02-07 08:48:37 +0700

    initial postgres

---
+++ src/test/java/id/perumdamts/cater/CaterApplicationTests.java
@@ -0,0 +1,13 @@
+package id.perumdamts.cater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
